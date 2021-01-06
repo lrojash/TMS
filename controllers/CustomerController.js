97017f44acdaae91755809025fa12153af4953bf
Luis Rojas <luisrohu91@gmail.com>
2021-01-05 22:35:12 -0700

    alt search return customer

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -4,22 +4,34 @@ const { Op, literal, fn, col } = require('sequelize')
 
 
 const GetCustomer = async (req, res) => {
-    
+
     console.log('inside controller', req.body)
-    try{
-        let customerId = req.body.customerId
-        console.log('after seeting it', customerId)
+    try {
+        if (req.body.customerID) {
+            let customerId = req.body.customerId
+            console.log('after seeting it', customerId)
+            let customer = await Customer.findOne({
+                where: {
+                    id: customerId
+                }
+            })
+            res.send(customer)
+        }
+        let { body } = req
+        const { firstName, lastName, dob } = body
         let customer = await Customer.findOne({
             where: {
-                id: customerId
+                firstName: firstName,
+                lastName: lastName,
+                dateOfBirth: dob
             }
         })
         res.send(customer)
-    } catch(error) {
-        throw error 
+    } catch (error) {
+        throw error
     }
 }
 
-module.exports={
+module.exports = {
     GetCustomer,
 }
